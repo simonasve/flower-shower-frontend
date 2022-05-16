aa286fa377a2efdf674cce999a74c499009c8ff4
Simonas Vedeckis <simvedeckis@gmail.com>
2022-05-16 21:39:22 +0300

    [FS-9] Added cart page and axios support

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -2,3 +2,9 @@ import Vue from 'vue'
 import axios from 'axios'
 
 Vue.prototype.$axios = axios
+
+
+const api = axios.create({ baseURL: 'https://localhost:8080' }) //Chnage to backend base URL
+Vue.prototype.$api = api
+
+export { axios, api }
